60ddad65ca162fbedcd4432bbf8ed4a72be42ce9
Easton Gorishek <ieaston26@gmail.com>
2018-05-11 16:44:09 -0700

    taking steps forward

--- objects.js
+++ objects.js
@@ -2,12 +2,12 @@
 
 // class constructor 
 class CookieLocation {
-    constructor(location, minCustPerHr, maxCustPerHr, avgSoldPerCust, hours) {
+    constructor(location, minCustPerHr, maxCustPerHr, avgSoldPerCust) {
         this.location = location;
         this.minCustPerHr = minCustPerHr;
         this.maxCustPerHr = maxCustPerHr;
         this.avgSoldPerCust = avgSoldPerCust;
-        this.hours = hours;
+        //this.hours = hours;
         
     }
     // returns total cookies needed per hour
@@ -18,11 +18,12 @@ class CookieLocation {
     }
 }
 // instantiating objects
-let pdxAirport = new CookieLocation('PDX Airport', 23, 65, 6.3);
-let pioneerSquare = new CookieLocation('Pioneer Square', 3, 24, 1.2);
-let powells = new CookieLocation('Powell\'s', 11, 38, 3.7);
-let stJohns = new CookieLocation('St. Johns', 20, 38, 2.3);
-let waterfront = new CookieLocation('Waterfront', 2, 16, 4.6);
-let newObject = new CookieLocation(location, min, max, cookies);
+const pdxAirport = new CookieLocation('PDX Airport', 23, 65, 6.3);
+const pioneerSquare = new CookieLocation('Pioneer Square', 3, 24, 1.2);
+const powells = new CookieLocation('Powell\'s', 11, 38, 3.7);
+const stJohns = new CookieLocation('St. Johns', 20, 38, 2.3);
+const waterfront = new CookieLocation('Waterfront', 2, 16, 4.6);
+
 // An array of all our objects
 const locationList = [pdxAirport, pioneerSquare, powells, stJohns, waterfront];
+
